6cd1ae9fadfdb4bd2cefffd0a111417e4c6e8ded
yusrilarzaqi <arzaqiyusril@gmail.com>
2022-05-30 19:17:35 +0700

    add new file 45 Function using Argument

---
+++ Tutorial Python New/45 Fungsi Dan Argument/main.py
@@ -0,0 +1,33 @@
+""" Fungsi Dengan Argument """
+
+"""
+def nama_fungsi():
+    Banan fungsi
+"""
+
+#
+# def hello_world(nama):
+#     print(f"hello {nama}")
+#
+#
+# hello_world("Yusril")
+#
+
+# Program tambah
+
+#
+# def tambah(angka1, angka2):
+#     hasil = angka1 + angka2
+#     print(f"{angka1} + {angka2} = {hasil}")
+#
+#
+# tambah(10, 20)
+
+
+def say_hi(mahasiswa):
+    for mhs in mahasiswa:
+        print(f"Yang terhomat {mhs}")
+
+
+mhs = ["yusril", "adam", "bimo", "dimas"]
+say_hi(mhs)
